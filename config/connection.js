211624f435bc1d32531ec4056b77817261d27893
J <torres.j.isaias@gmail.com>
2021-09-18 09:12:26 -0500

    added heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+// create connection to our database, pass in your MySQL information for username and password
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
